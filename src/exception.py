5864294756c9381737318f09e4bed9c13c7443a1
Chetan Chintala <chetanchintala945@gmail.com>
2026-05-19 14:33:57 +0530

    Made custom exception handler for project

--- src/exception.py
+++ src/exception.py
@@ -1,6 +1,26 @@
 # responsible for exceptopn handling
-import os
 import sys # contains all exceptions/errors occuring in project
 
 def error_handler(error,error_detail:sys):
-    _,_,exc_tb=error_detail.exc_info()
+    _,_,exc_tb=error_detail.exc_info() # gives info about the cur exception.
+    '''
+    Example =
+        try:
+            a = 1 / 0
+        except Exception as e:
+            error_handler(e, sys)
+    '''
+    file_name=exc_tb.tb_frame.f_code.co_filename
+    error_message = "Error occured in python script name [{0}] line number [{1}] error message [{2}]".format(
+    file_name,
+    exc_tb.tb_lineno,
+    str(error)
+    )
+    return error_message
+
+class CustomException(Exception):
+    def __init__(self,error_message,error_detail:sys):
+        super.__init__(error_message)
+        self.error_messaage=error_handler(error_message,error_detail)
+    def __str__(self):
+        return self.error_message
